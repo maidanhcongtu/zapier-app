91cbd7c236ae27b63c606962f87d7471d1b4e27a
danh mai <danh.mai@axonactive.com>
2019-04-19 11:52:58 +0700

    Add hellow api

---
+++ src/main/java/com/mhdanh/zapier/APIConfiguration.java
@@ -0,0 +1,9 @@
+package com.mhdanh.zapier;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class APIConfiguration extends Application {
+
+}
---
+++ src/main/java/com/mhdanh/zapier/api/HelloworldAPI.java
@@ -0,0 +1,13 @@
+package com.mhdanh.zapier.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("hello")
+public class HelloworldAPI {
+
+	@GET
+	public String sayHello() {
+		return "hello world";
+	}
+}
