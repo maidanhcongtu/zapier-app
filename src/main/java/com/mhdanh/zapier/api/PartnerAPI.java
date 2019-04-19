b55239b7e198c427fe0debcbb8e9e0367a6347bb
danh mai <danh.mai@axonactive.com>
2019-04-19 17:05:45 +0700

    add produce application/json

--- src/main/java/com/mhdanh/zapier/api/PartnerAPI.java
+++ src/main/java/com/mhdanh/zapier/api/PartnerAPI.java
@@ -3,15 +3,20 @@ package com.mhdanh.zapier.api;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.mhdanh.zapier.data.PartnerRepo;
 import com.mhdanh.zapier.model.Partner;
 
 @Path("{tenant}/partners")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class PartnerAPI {
 	
 	@Inject
