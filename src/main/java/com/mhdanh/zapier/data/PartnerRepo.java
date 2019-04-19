912a8597d89f77ab6fc6ee6bdb17688e7595bfb4
danh mai <danh.mai@axonactive.com>
2019-04-19 18:29:43 +0700

    Reverse order

--- src/main/java/com/mhdanh/zapier/data/PartnerRepo.java
+++ src/main/java/com/mhdanh/zapier/data/PartnerRepo.java
@@ -1,6 +1,7 @@
 package com.mhdanh.zapier.data;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -15,7 +16,9 @@ public class PartnerRepo {
 	private List<Partner> partners = new ArrayList<>();
 	
 	public List<Partner> getAll(String tenant) {
-		return partners.stream().filter(p -> p.getTenant().equalsIgnoreCase(tenant)).collect(Collectors.toList());
+		List<Partner> partnes = partners.stream().filter(p -> p.getTenant().equalsIgnoreCase(tenant)).collect(Collectors.toList());
+		Collections.reverse(partnes);
+		return partnes;
 	}
 	
 	public Partner create(Partner partner) {
